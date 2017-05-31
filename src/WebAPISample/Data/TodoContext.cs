5a057a6ba7b42483aa430a93347dd741339ad07c
Chien <chien@hdvietnam.com.vn>
2017-05-31 16:36:03 +0700

    Complete

---
+++ src/WebAPISample/Data/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPISample.Models;
+
+namespace WebAPISample.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}

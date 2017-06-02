6d141c944eb43162971a63938e1ee1361fd3d91a
Chien <chien@hdvietnam.com.vn>
2017-06-02 11:20:31 +0700

    separated project to test webApi

--- src/OnAir/Controllers/OnAirController.cs
+++ src/OnAir/Controllers/OnAirController.cs
@@ -8,24 +8,31 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using OnAir.Models;
 using OnAirServices;
-
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace OnAir.Controllers
 {
     public class OnAirController : Controller
     {
         private readonly HDStationPS_V4Context _context;
-        private readonly Sector _pi;        
+        private readonly Sector _pi;
+        static HttpClient client;
 
         public OnAirController(HDStationPS_V4Context context, IOptions<Sector> pi)
         {
             _pi = pi.Value;
             _context = context;
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:65000/AutomationAPI");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        public IActionResult Index()
-        {            
-            var items = GetPlaylistItems(GetPlaylist(_pi.SectorID).FirstOrDefault().ListId );
+        public async Task<IActionResult> Index()
+        {
+            string x = await GetPlaylistAsync();
+            var items = GetPlaylistItems(GetPlaylist(_pi.SectorID).FirstOrDefault().ListId);
             var lstItems = new List<ItemViewModel>();
             foreach (var temp in items)
             {
@@ -41,7 +48,18 @@ namespace OnAir.Controllers
                 });
             }
             return View(lstItems);
-        }    
+        }
+        public async Task<string> GetPlaylistAsync()
+        {
+            string test = "";
+            string path = "http://localhost:65000/AutomationAPI/Playlist/1";
+         HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                test = await response.Content.ReadAsStringAsync();
+            }
+            return test;
+        }
         public List<PlayList> GetPlaylist(int sectorId)
         {
             return _context.PlayList.Where(m => m.DateList == DateTime.Now.Date).ToList();
